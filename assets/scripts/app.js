d2e6b0017d90af11c24d2b9246e2c9ea0851078b
moimudi <62672543+moimudi@users.noreply.github.com>
2020-08-09 20:38:22 +0200

    26. Logical Operators - A Quick Summary

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -215,3 +215,21 @@ logBtn.addEventListener('click', printLogHandler);
 // true
 // isLoggedIn && ''
 // ""
+
+// onst userName = 'Max';
+// const altName = '';
+// console.log(userName === 'Max'); // generates and prints a boolean => true
+// console.log(userName); // wasn't touched, still is a string => 'Max'
+
+// console.log(userName || null); // userName is truthy and therefore returned by || => 'Max'
+// console.log(altName || 'Max'); // altName is falsy (empty string), hence 'Max' is returned => 'Max'
+// console.log(altName || ''); // both altName and '' are falsy but if the first operand is falsy, the second one is always returned => ''
+// console.log(altName || null || 'Anna'); // altName and null are falsy, 'Anna' is returned => 'Anna'
+
+// console.log(userName && 'Anna'); // userName is truthy, hence second (!) value is returned => 'Anna'
+// console.log(altName && 'Anna'); // altName is falsy, hence first value is returned => ''
+// console.log(userName && ''); // userName is truthy, hence second value is returned => ''
+
+// const enteredValue = ''; // let's assume this is set based on some input provided by the user, therefore it might be an empty string
+// const userName = enteredValue || 'PLACEHOLDER'; // will assign 'PLACEHOLDER' if enteredValue is an empty string
+// console.log(userName);
