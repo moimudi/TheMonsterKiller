28936a26dbb628762ec0398a73438319a05a1e36
moimudi <62672543+moimudi@users.noreply.github.com>
2020-08-09 22:38:24 +0200

    31. The for-of Loop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -217,16 +217,22 @@ function printLogHandler() {
     console.log('Yolla');
   }
   for (let i = 0; i < battleLog.length; i++) {
-    console.log('Tallinn');
+    console.log(battleLog[i]);
   }
-  for (let i = 10; i > 0; i--) {
+  let i = 0;
+  for (const logEntry of battleLog) {
+    console.log(logEntry);
     console.log(i);
+    i++;
   }
-  for (let i = 10; i > 0; ) {
-    i--;
-    console.log(i);
-  }
-  console.log(battleLog);
+  // for (let i = 10; i > 0; i--) {
+  //   console.log(i);
+  // }
+  // for (let i = 10; i > 0; ) {
+  //   i--;
+  //   console.log(i);
+  // }
+  // console.log(battleLog);
 }
 
 attackBtn.addEventListener('click', attackHandler);
